e52562c89316a8fc8b9eb4143fe6441b67b87e52
Aralov7 <154493935+Aralov7@users.noreply.github.com>
2024-11-19 02:37:31 +0300

    шиза

--- backend/02.ООП/3. Требование к коду/1.py
+++ backend/02.ООП/3. Требование к коду/1.py
@@ -1,26 +1,7 @@
-from math import sqrt
-from typing import Optional
-
-
-def add_numbers(first: int, second: int) -> int:
+def add(first, second):
     return first + second
 
 
-def calculate_square_root(number: int) -> float:
-    return sqrt(number)
-
-
-def calc(your_number: int) -> Optional[str]:
-    if your_number <= 0:
-        return
-    num = calculate_square_root(your_number)
-    return ('Мы вычислили квадратный корень из введённого вами числа. '
-            f'Это будет: {num}')
-
-
-x1 = 10
-y1 = 5
-
-print('Сумма чисел: ', add_numbers(x1, y1))
-
-print(calc(25.5))
+first = 10
+second = 5
+print('Сумма чисел: ', add(first, second))
